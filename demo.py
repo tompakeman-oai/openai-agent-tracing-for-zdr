578e1692b08c94175293aea1df51f46b3d82ccb0
Tom Pakeman <tompakeman@openai.com>
2025-07-31 13:35:50 +0100

    Working first version

---
+++ demo.py
@@ -0,0 +1,37 @@
+import asyncio
+from openai import OpenAI
+from agents import Agent, Runner, trace, function_tool
+from agents.tracing import set_trace_processors
+from local_tracer import LocalTraceProcessor
+from datetime import datetime
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+client = OpenAI()
+
+@function_tool
+def web_search(query: str) -> str:
+    """Search the web for information and return back a summary of the results"""
+    global client
+    result = client.responses.create(
+        model="gpt-4o-mini",
+        input=f"Search the web for '{query}' and reply with only the result.",
+        tools=[{"type": "web_search_preview"}],
+    )
+    return result.output_text
+
+agent = Agent(
+    name="Test agent",
+    model="o3",
+    instructions="Reason and answer the question.",
+    tools=[web_search],
+)
+async def main():
+    set_trace_processors([LocalTraceProcessor()])
+    with trace("Testing", metadata={"timestamp": datetime.now().isoformat()}) as t:
+        result = await Runner.run(agent, input=f"What's the latest news (as of {t.metadata['timestamp']}) from OpenAI?")
+        print(result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
